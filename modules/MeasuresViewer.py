1bf71189ee9b9d3562344f046aa07d03d1d57c9c
Samuel Hancak <79135406+SamuelHancak@users.noreply.github.com>
2024-02-23 12:48:13 +0100

    Added mean and standard deviation to the similarity measures table

--- modules/MeasuresViewer.py
+++ modules/MeasuresViewer.py
@@ -19,6 +19,12 @@ class MeasuresViewer:
         if self.similarity_measures.index.name is None:
             self.similarity_measures.index.name = ""
 
+        means = self.similarity_measures.mean()
+        std_devs = self.similarity_measures.std()
+
+        mean_val = ["Mean"] + list(means)
+        stdDev_val = ["Standard deviation"] + list(std_devs)
+
         columns = [self.similarity_measures.index.name] + list(
             self.similarity_measures.columns
         )
@@ -30,8 +36,15 @@ class MeasuresViewer:
         )
 
         for col in columns:
-            tree.heading(col, text=col, command=lambda c=col: self.sort_tree(tree, c))
+            tree.heading(
+                col,
+                text=col,
+                command=lambda c=col: self.sort_tree(tree, c, True),
+            )
             tree.column(col, anchor="center")
+
+        tree.insert("", "end", values=mean_val)
+        tree.insert("", "end", values=stdDev_val)
 
         for index, row in self.similarity_measures.iterrows():
             tree.insert("", "end", values=[index] + list(row))
@@ -51,7 +64,11 @@ class MeasuresViewer:
         )
 
         for col in graphlet_counts.columns:
-            tree.heading(col, text=col, command=lambda c=col: self.sort_tree(tree, c))
+            tree.heading(
+                col,
+                text=col,
+                command=lambda c=col: self.sort_tree(tree, c, False),
+            )
             tree.column(col, anchor="center")
 
         for _, row in graphlet_counts.iterrows():
@@ -59,18 +76,47 @@ class MeasuresViewer:
 
         tree.pack(fill="both", expand=True)
 
-    def sort_tree(self, tree, column, reverse=False):
-        current_data = [
+    def sort_tree(self, tree, column, is_similarity, reverse=False):
+        data_to_sort = [
             (tree.set(child, column), child) for child in tree.get_children("")
         ]
+
+        mean_row = None
+        std_dev_row = None
+        data_to_sort_excluding_mean_std = []
+
+        if is_similarity:
+            for value, child in data_to_sort:
+                if tree.index(child) == 0:
+                    mean_row = (value, child)
+                elif tree.index(child) == 1:
+                    std_dev_row = (value, child)
+                else:
+                    data_to_sort_excluding_mean_std.append((value, child))
+        else:
+            data_to_sort_excluding_mean_std = data_to_sort
+
         try:
             sorted_data = sorted(
-                current_data, key=lambda x: float(x[0]), reverse=reverse
+                data_to_sort_excluding_mean_std,
+                key=lambda x: float(x[0]),
+                reverse=reverse,
             )
         except ValueError:
-            sorted_data = sorted(current_data, key=lambda x: x[0], reverse=reverse)
+            sorted_data = sorted(
+                data_to_sort_excluding_mean_std, key=lambda x: x[0], reverse=reverse
+            )
+
+        if mean_row:
+            sorted_data.insert(0, mean_row)
+
+        if std_dev_row:
+            sorted_data.insert(1, std_dev_row)
 
         for index, item in enumerate(sorted_data):
             tree.move(item[1], "", index)
 
-        tree.heading(column, command=lambda: self.sort_tree(tree, column, not reverse))
+        tree.heading(
+            column,
+            command=lambda: self.sort_tree(tree, column, is_similarity, not reverse),
+        )
