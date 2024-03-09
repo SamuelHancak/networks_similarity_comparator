6dfa1b59c6ba277dbb73f5814704cfe2aa058af3
Samuel Hancak <79135406+SamuelHancak@users.noreply.github.com>
2024-03-09 17:09:20 +0100

    Updated rgfd function

--- modules/NetworkDistances.py
+++ modules/NetworkDistances.py
@@ -10,36 +10,22 @@ class NetworkDistances:
         self.orbit_counts_df = orbit_counts_df
         self.similarity_measures_df = pd.DataFrame()
         self.column_combinations = list(combinations(self.orbit_counts_df.columns, 2))
-        self.orbit_counts_log_normal = DataNormaliser(
-            orbit_counts_df
-        ).log_scale_percentual_normalisation()
         self.orbit_counts_percentual_normal = DataNormaliser(
             orbit_counts_df
         ).percentual_normalisation()
 
     def computeRGFDist(self):
-        computations = (
-            DataNormaliser(self.orbit_counts_df)
-            .log_scale_percentual_normalisation()
-            .apply(
-                lambda col: col.map(
-                    lambda val: (-1 * (np.log10(val) if val > 0 else 0))
-                    / (np.log10(col.sum()) if col.sum() > 1 else 1)
-                )
-            )
+        computations = self.orbit_counts_percentual_normal.apply(
+            lambda col: col.map(lambda val: (-1 * (np.log10(val) if val > 0 else 0)))
         )
 
         result_df = pd.DataFrame()
         for col1, col2 in self.column_combinations:
-            zero_cols = computations.loc[
-                (computations[col1] == 0) | (computations[col2] == 0)
-            ].index.to_numpy()
+            distance = np.abs(computations[col2] - computations[col1])
+            zero_mask = (computations[col1] == 0) | (computations[col2] == 0)
+            distance.loc[zero_mask] = 0
 
-            result_df[col2 + "---" + col1] = abs(
-                computations[col2] - computations[col1]
-            )
-            if len(zero_cols) > 0:
-                result_df.iloc[zero_cols, result_df.shape[1] - 1] = 0
+            result_df[col2 + "---" + col1] = distance
 
         self.similarity_measures_df["RGFDist"] = result_df.sum()
 
